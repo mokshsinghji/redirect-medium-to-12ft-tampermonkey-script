c74bf258827f41071fafd74cedb94a3c8575f50e
mokshsinghji <77796854+mokshsinghji@users.noreply.github.com>
2023-05-20 11:39:58 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*medium.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=telegram.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location = "https://12ft.io/" + window.location;
+    // Your code here...
+})();
